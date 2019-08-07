32a6f98f4730729b72158fa5543f6d271d009863
CCEClass1 <CCEstdt21>
2019-08-07 15:39:04 -0600

    Day 3 info

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -2,7 +2,7 @@
 List comprehensions
 Readable, expressive, and effective
 """
-from math import factorial
+from math import factorial, sqrt
 
 
 def main():
@@ -26,10 +26,40 @@ def main():
         # puts factorial into list
         info.append(len(str(factorial(x))))
     # print info
-    print(info)
+    print(info, type(info))
     # Use a list comprehension instead: [ ]
     info2 = [len(str(factorial(x))) for x in range(20)]
-    print(info2)
+    print(info2, type(info2))
+
+    # Set comprehensions: { }
+    info3 = {len(str(factorial(x))) for x in range(20)}
+    print(info3, type(info3))
+
+    # Dictionary comprehensions: { }
+    nba_teams = {'Jazz':'SLC', 'Warriors':'Oakland', 'Lakers':'LA', 'clippers':'LA'}
+    print(nba_teams)
+    teams_nba = {city:mascot for mascot, city in nba_teams.items()}
+    print(teams_nba)
+
+    # Filter predicates
+    def is_prime(num):
+        """
+        Determine if the number is prime
+        :param num: input n
+        :return: True for prime numbers
+                False for non-prime numbers
+        """
+        if num < 2:
+            return False
+        for i in range(2, int(sqrt(num))+1):
+            if num % i == 0:
+                return False
+        return True
+    primes = [x for x in range(1001) if is_prime(x)]
+    print(len(primes), primes)
+
+
+
 
 
 
