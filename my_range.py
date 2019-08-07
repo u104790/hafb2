18a652e6049aebb1734fefa2b0276c085cb78384
CCEClass1 <CCEstdt21>
2019-08-07 08:48:35 -0600

    Day 2 info

---
+++ my_range.py
@@ -0,0 +1,44 @@
+"""
+Learn about range() collection
+"""
+
+def main():
+    """
+    Test function
+    :return: Nothing
+    """
+    # Default
+    for i in range(5):
+        print(i)
+
+    # Set the initial value:
+    print("Now setting initial value")
+    for i in range(5,10):
+        print(i)
+
+    # Create a list from range
+    l = list(range(5,10))
+    print(l, type(l))
+    l2 = list(range(5,10)) + list(range(30,40))
+    print(l2, type(l2))
+    # Step argument: (begin, stop (which is not included), step)
+    l3 = list(range(0, 20, 2))
+    print(l3, type(l3))
+
+    # Iteration over list using index notation
+    s=[0, 2, 3, 4, 6]
+    for i in range(len(s)):
+        print(s[i])
+    # Better way
+    for v in s:
+        print(v)
+
+    # enumerate(): returns an iterable series
+    t = [6, 789, 123, 98, 3, 22]
+    for p in enumerate(t):
+        print(p)
+
+
+if __name__ == '__main__':
+    main()
+    exit(0)
